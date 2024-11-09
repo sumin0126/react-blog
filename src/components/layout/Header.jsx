3b03b447be72fe35793413f5e790d448b53e225c
sumin0126 <ktnals2@naver.com>
2024-11-09 15:15:59 +0900

    feat: Header 조건부 렌더링 추가
    Home 인 경우에만 글쓰기, 검색 보이게 수정

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 /**
  * @description 헤더
  */
 const Header = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleClickLogo = () => {
     navigate('/');
@@ -15,21 +16,24 @@ const Header = () => {
     navigate('/post/new');
   };
 
+  const isHome = location.pathname === '/';
+
   return (
     <header>
       <div className="headerBar">
         <h2 onClick={handleClickLogo}>smooth.log</h2>
+        {isHome && (
+          <div className="header_right">
+            <div className="searchInput">
+              <i className="fa-solid fa-search"></i>
+              <input placeholder="검색어를 입력하세요"></input>
+            </div>
 
-        <div className="header_right">
-          <div className="searchInput">
-            <i className="fa-solid fa-search"></i>
-            <input placeholder="검색어를 입력하세요"></input>
+            <div className="writingBtn" onClick={handleClickWriting}>
+              ✐ 글쓰기
+            </div>
           </div>
-
-          <div className="writingBtn" onClick={handleClickWriting}>
-            ✐ 글쓰기
-          </div>
-        </div>
+        )}
       </div>
     </header>
   );
