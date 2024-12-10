c4e87c364902c171b0318a3a4ad2b2cd8992e849
sumin0126 <ktnals2@naver.com>
2024-12-10 21:16:53 +0900

    fix: 글쓰기 페이지 디자인 변경

--- src/pages/Writing.jsx
+++ src/pages/Writing.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 
@@ -6,7 +6,6 @@ import { getDate } from 'utils/common';
 import AlertModal from 'components/modal/AlertModal';
 import { REVERSE_CATEGORY } from 'constants/navbar2';
 import { CATEGORY } from 'constants/navbar';
-import { PATHNAME } from 'constants/common';
 
 /**
  * @description - 글 수정 및 등록 컴포넌트
@@ -23,8 +22,10 @@ const Writing = () => {
   });
   const [modalTitle, setModalTitle] = useState('');
   const [isOpenModal, setIsOpenModal] = useState(false);
-  const [isOpenCategory, setIsOpenCategory] = useState(false);
   const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [fileName, setFileName] = useState('');
+  const titleInputRef = useRef();
 
   const navigate = useNavigate();
   const location = useLocation();
@@ -54,46 +55,43 @@ const Writing = () => {
     setPost({ ...post, content: newContent });
   };
 
-  // 클릭한 카테고리를 새 category로 저장해주는 함수
-  const handleChangeCategory = (e) => {
-    const newCategory = e.target.textContent;
-    setPost({ ...post, category: REVERSE_CATEGORY[newCategory] });
+  // 모달 타이틀을 받아서 상태를 업데이트해주고 모달창을 띄워주는 함수
+  const createModal = (title) => {
+    setModalTitle(title);
+    setIsOpenModal(true);
   };
 
-  // 수정된 글 저장 및 완료 모달 띄워주는 함수
-  // 등록된 글 전송 및 완료 모달 띄워주는 함수
-  // 카테고리 미선택시 모달 띄워주는 함수
+  // 수정된 글을 서버에 저장해주는 함수
+  // 등록된 글을 서버에 전송해주는 함수
   const handleUpload = () => {
     if (post.title === '' || post.content === '') {
-      setModalTitle('제목과 내용을 입력하세요 !');
-      setIsOpenModal(true);
+      createModal('제목과 내용을 입력하세요 !');
       return null;
     }
 
     if (isEdit) {
       axios.put(`http://localhost:3001/posts/${id}`, post).then(() => {
-        setModalTitle('수정이 완료 되었습니다 !');
-        setIsOpenModal(true);
+        createModal('수정이 완료되었습니다 !');
       });
     } else {
       const categoryTitle = document.querySelector('.category');
 
       if (categoryTitle.textContent === '카테고리') {
-        setModalTitle('카테고리를 선택하세요 !');
-        setIsOpenModal(true);
-        navigate(PATHNAME.POST_NEW);
+        createModal('카테고리를 선택하세요 !');
       } else {
         axios.post('http://localhost:3001/posts', post).then(() => {
-          setModalTitle('등록이 완료 되었습니다 !');
-          setIsOpenModal(true);
+          createModal('등록이 완료되었습니다 !');
         });
       }
     }
   };
 
-  // 썸네일 이미지 업로드
+  // 썸네일 이미지 업로드해주는 함수
   const handleUploadImage = (e) => {
     const file = e.target.files[0];
+    if (file) {
+      setFileName(file.name);
+    }
 
     const fileReader = new FileReader();
     fileReader.readAsDataURL(file);
@@ -115,33 +113,17 @@ const Writing = () => {
     });
   }, []);
 
-  // 카테고리 버튼을 누르면 카테고리 메뉴가 열리는 함수
-  const handleClickCategory = (e) => {
-    e.stopPropagation();
-    setIsOpenCategory(true);
+  // 클릭한 카테고리를 새 category로 저장해주는 함수
+  const handleChangeCategory = (e) => {
+    const newCategory = e.target.textContent;
+    setPost({ ...post, category: REVERSE_CATEGORY[newCategory] });
+    setSelectedCategory(newCategory);
   };
 
-  // 화면 클릭 시 카테고리 창을 닫아주는 함수
+  // 제목 인풋창에 커서를 활성화 시켜주는 함수
   useEffect(() => {
-    const closeCategory = () => {
-      setIsOpenCategory(false);
-    };
-
-    document.addEventListener('click', closeCategory);
-
-    return () => {
-      document.removeEventListener('click', closeCategory);
-    };
+    titleInputRef.current.focus();
   }, []);
-
-  // 카테고리 메뉴에서 원하는 카테고리를 선택하면 화면에 보여주는 함수
-  const handleClickSelectCategory = (e) => {
-    const selectCategory = e.target.textContent;
-    const category = document.querySelector('.category-title');
-
-    category.textContent = selectCategory;
-    setIsOpenCategory(false);
-  };
 
   return (
     <>
@@ -151,70 +133,98 @@ const Writing = () => {
           completeText="확인"
           onClickComplete={() => {
             setIsOpenModal(false);
-            navigate(-1);
+            {
+              (modalTitle.includes('수정') || modalTitle.includes('등록')) &&
+                navigate(-1);
+            }
           }}
           modalClose={() => setIsOpenModal(false)}
         />
       )}
+
+      {/* 글쓰기 페이지 전체 래퍼 */}
       <section className="writing-wrapper">
-        <div>
-          <input type="file" accept="image/*" onChange={handleUploadImage} />
-        </div>
-        <div className="category" onClick={handleClickCategory}>
-          <p className="category-title">카테고리</p>
-          <i className="fa-solid fa-sort-down bottom-arrow"></i>
-        </div>
-
-        {isOpenCategory && (
-          <div className="category-dropdown" onClick={handleChangeCategory}>
-            {categories
-              .filter((category) => category.categoryName !== 'all')
-              .map((category) => (
-                <div
-                  className="category-title-box"
-                  onClick={handleClickSelectCategory}
-                  key={category.id}
-                >
-                  <i className="fa-regular fa-file file-icon"></i>
-                  <p>{CATEGORY[category.categoryName]}</p>
-                </div>
-              ))}
-          </div>
-        )}
-
-        <div className="text-wrapper">
-          <input
-            className="title"
-            placeholder="제목을 입력하세요"
-            value={post.title}
-            onChange={handleChangeTitle}
-          />
-
-          {post.thumbnail && (
-            <img
-              src={post.thumbnail}
-              alt="thumbnail"
-              width={200}
-              height={200}
+        {/* 텍스트와 사이드를 관리하는 래퍼 */}
+        <section className="text-side-wrapper">
+          {/* 제목/내용 인풋창 래퍼 */}
+          <section className="text-wrapper">
+            <input
+              className="title"
+              placeholder="제목을 입력하세요"
+              value={post.title}
+              onChange={handleChangeTitle}
+              ref={titleInputRef}
             />
-          )}
 
-          <textarea
-            className="content"
-            placeholder="내용을 입력하세요"
-            value={post.content}
-            onChange={handleChangeContent}
-          />
-        </div>
+            {post.thumbnail && (
+              <img
+                src={post.thumbnail}
+                alt="thumbnail"
+                width={200}
+                height={200}
+              />
+            )}
 
-        <div className="btn-wrapper">
-          <button className="uploadBtn" onClick={handleUpload}>
-            {isEdit ? '수정' : '등록'}
-          </button>
-          <button className="cancelBtn" onClick={handleClickCancel}>
-            취소
-          </button>
-        </div>
+            <textarea
+              className="content"
+              placeholder="내용을 입력하세요"
+              value={post.content}
+              onChange={handleChangeContent}
+            />
+          </section>
+
+          <section className="side-wrapper">
+            {/* 카테고리 버튼창 */}
+            <div className="category">
+              <p className="category-title">카테고리를 선택하세요 !</p>
+
+              <div className="category-list" onClick={handleChangeCategory}>
+                {categories
+                  .filter((category) => category.categoryName !== 'all')
+                  .map((category) => (
+                    <div
+                      className={`category-list-title
+                        ${selectedCategory === CATEGORY[category.categoryName] ? ' active' : ''}`}
+                      key={category.id}
+                      onClick={(e) => handleChangeCategory(e)}
+                    >
+                      <i className="fa-regular fa-file category-file-icon"></i>
+                      <p className="category-name">
+                        {CATEGORY[category.categoryName]}
+                      </p>
+                    </div>
+                  ))}
+              </div>
+            </div>
+
+            {/* 이미지 인풋창 */}
+            <div className="image-input">
+              <input
+                className="file-upload-name"
+                value={fileName}
+                readOnly
+                placeholder="첨부파일"
+              />
+              <label for="file">파일찾기</label>
+              <input
+                type="file"
+                id="file"
+                accept="image/*"
+                onChange={handleUploadImage}
+              />
+            </div>
+
+            {/* 버튼 래퍼 */}
+            <section className="btn-wrapper">
+              <button className="uploadBtn" onClick={handleUpload}>
+                {isEdit ? '수정' : '등록'}
+              </button>
+              <button className="cancelBtn" onClick={handleClickCancel}>
+                취소
+              </button>
+            </section>
+          </section>
+        </section>
       </section>
     </>
   );
